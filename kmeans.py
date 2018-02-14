8b9422567aa80d75636ce981f0faf27573f97b2b
Diego Hamilton <diegomhamilton@github.com>
2018-02-14 19:23:38 -0300

    Remove unnecessary parts, clean code

--- kmeans.py
+++ kmeans.py
@@ -7,12 +7,10 @@ import partitionset as ps
 import moore as m
 import numpy as np
 import random
+import sequence_generator as sg
 
 
-def generate_sequence(D, L, K, moore_iter = -1, name = 'ternary_even_shift'):
-    with open('../dcgram_files/{}/results/machines/dmarkov/dmark_{}.yaml'.format(name, D), 'r') as f:
-         machine = yaml.load(f)
-
+def clusterize(machine, L, D, K, moore_iter = -1, name = 'ternary_even_shift'):
     all_oedges = [state.outedges for state in machine.states]
     morphs = []
 
@@ -45,25 +43,37 @@ def generate_sequence(D, L, K, moore_iter = -1, name = 'ternary_even_shift'):
     for i in range(len(morphs)):
         clusters[kmeans.labels_[i]].append(machine.states[i])
 
-    initial_partition = []
+    initial_pt = []
 
     for p in clusters:
-        partition = pt.Partition()
+        pt = pt.Partition()
         for state in p:
-            partition.add_to_partition(state)
-        initial_partition.append(partition)
+            pt.add_to_pt(state)
+        initial_pt.append(pt)
 
+    initial_pt = ps.PartitionSet(initial_pt)
+    final_pt = m.moore_by_parts(initial_pt, machine, n_iter = moore_iter)
 
-    initial_partition = ps.PartitionSet(initial_partition)
-    final_partition = m.moore_by_parts(initial_partition, machine, n_iter = moore_iter)
+    if (moore_iter == -1):
+        with open('../dcgram_files/{}/results/machines/dcgram/before_redefine/\
+                    dcgram_D{}_K{}.yaml'.format(name, D, K), 'w') as f:
+            yaml.dump(final_pt, f)
+    else:
+        with open('../dcgram_files/{}/results/machines/dcgram/before_redefine/\
+                    dcgram_D{}_K{}_iter{}.yaml'.format(name, D, K, moore_iter), 'w') as f:
+            yaml.dump(final_pt, f)
 
-    with open('../dcgram_files/{}/results/machines/dcgram/dcgram_D{}_K{}.yaml'.format(name, D, K), 'w') as f:
-        yaml.dump(final_partition, f)
+    new_pt = final_pt.redefine_partition()
+    if (moore_iter == -1):
+        with open('../dcgram_files/{}/results/machines/dcgram/dcgram_D{}_K{}.yaml'\
+        .format(name, D, K), 'w') as f:
+            yaml.dump(new_pt, f)
+    else:
+        with open('../dcgram_files/{}/results/machines/dcgram/dcgram_D{}_K{}_iter{}.yaml'\
+        .format(name, D, K, moore_iter), 'w') as f:
+            yaml.dump(new_pt, f)
 
-    for p in final_partition.partitions:
-        for i in range(len(p.name)):
-            print('name:{}|oedge:{}'.format(p.name[i],p.outedges[i]))
-        print('--------------\n')
+    return new_pt
 
     # class kmeans(KMeans):
     #     def __init__(self, machine, n, r):
@@ -85,62 +95,3 @@ def generate_sequence(D, L, K, moore_iter = -1, name = 'ternary_even_shift'):
     #                 curr_morph[int(label)] = oedge[-1]
     #             morphs.append(curr_morph)
     #         return morphs
-
-    #?
-    #BEGINNING
-    avg_morphs = []
-
-    for p in final_partition.partitions:
-        morphs = []
-
-        for oedges in p.outedges:
-            curr_morph = [0, 0, 0]
-            for oedge in oedges:
-                label = oedge[0]
-                curr_morph[int(label)] = oedge[-1]
-            morphs.append(curr_morph)
-
-        means = []
-
-        for i in range(len(curr_morph)):
-            means.append(np.mean([m[i] for m in morphs]))
-
-        avg_morphs.append(means)
-    # END
-
-    #PUT ON sequencegenerator.py
-    #BEGINNING
-    sequence = ''
-    curr_partition = 0
-
-    for i in range(L):
-        # Set data parameters
-        labels = [str(label) for label in range(len(avg_morphs[curr_partition]))]
-        probabilities = avg_morphs[curr_partition]
-        # Weight formatting
-        for weight in probabilities:
-            weight = int(weight * 10e16)
-        # Chooses next state
-        label = random.choices(labels, probabilities)[0]
-        sequence = sequence + label
-        #CREATE FUNCTION ON partitionset.py CALLED find_next_partition
-        #BEGINNING
-        for oedge in (final_partition.partitions[curr_partition]).outedges[0]:
-            if oedge[0] == label:
-                state = oedge[1] #name of next state (gets first outedge in partition)
-        j = 0
-        breaker = False
-        for p in final_partition.partitions:
-            for state_named in p.name:
-                if state_named == state:
-                    curr_partition = j
-                    breaker = True
-                    break
-            if breaker:
-                break
-            else:
-                j += 1
-        #END
-    #END
-
-    return sequence
