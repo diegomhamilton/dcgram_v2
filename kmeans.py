921cf1de5fec2d072690f2652ad68fbd0d5f15b2
Diego Hamilton <diegomhamilton@github.com>
2018-03-15 21:57:25 -0300

    Save files with label for Moore not-default iterations

--- kmeans.py
+++ kmeans.py
@@ -10,10 +10,16 @@ import random
 import sequence_generator as sg
 
 
-def clusterize(machine, L, D, K, moore_iter = -1, name = 'ternary_even_shift'):
+def clusterize(machine, L, D, K, moore_iter = -1, name = 'ternary_even_shift', save_plot = True):
     all_oedges = [state.outedges for state in machine.states]
     morphs = []
+    #define label for files if not default Moore's algorithm execution
+    if moore_iter != -1:
+        moore_label = '_moore_{}_iter'.format(moore_iter)
+    else:
+        moore_label = ''
 
+    # Normalize morphs
     for oedges in all_oedges:
         curr_morph = [0, 0, 0]
         for oedge in oedges:
@@ -22,25 +28,23 @@ def clusterize(machine, L, D, K, moore_iter = -1, name = 'ternary_even_shift'):
         morphs.append(curr_morph)
 
     kmeans = k(n_clusters = K, random_state = 0).fit(morphs)
-
     clusters = [[] for i in range(kmeans.n_clusters)]
-
     for i in range(len(morphs)):
         clusters[kmeans.labels_[i]].append(morphs[i])
+    if save_plot:
+        idx = 0
+        plot_label = ['bo','g*','c^', 'mo', 'y*', 'k^', 'ro']
+        for c in clusters:
+            plt.plot([x[0] for x in c], [y[1] for y in c], plot_label[idx])
+            idx += 1
 
-    idx = 0
-    plot_label = ['bo','g*','c^', 'mo', 'y*', 'k^', 'ro']
-    for c in clusters:
-        plt.plot([x[0] for x in c], [y[1] for y in c], plot_label[idx])
-        idx += 1
-
-    plt.plot([x[0] for x in kmeans.cluster_centers_], [y[1] for y in kmeans.cluster_centers_], 'r+')
-    plt.axis([0, 1.0, 0, 1.0])
-    plt.title('Clusters of morphs for K = {}'.format(K))
-    plt.ylabel('$P(0)$')
-    plt.xlabel('$P(1)$')
-    plt.savefig('../dcgram_files/{}/results/plots/kmeans_dmark_D{}_K{}_clusters.png'.format(name, D, K))
-    plt.gcf().clear()
+        plt.plot([x[0] for x in kmeans.cluster_centers_], [y[1] for y in kmeans.cluster_centers_], 'r+')
+        plt.axis([-0.05, 1.05, -0.05, 1.05])
+        plt.title('Agrupamento de morphs para D = {}, K = {}'.format(D, K))
+        plt.ylabel('$P(0)$')
+        plt.xlabel('$P(1)$')
+        plt.savefig('../dcgram_files/{}/results/plots/kmeans_dmark_D{}_K{}_clusters{}.png'.format(name, D, K, moore_label))
+        plt.gcf().clear()
     #----------------------------------------------------------------------------------
     #MOORE
     clusters = [[] for i in range(kmeans.n_clusters)]
@@ -59,22 +63,14 @@ def clusterize(machine, L, D, K, moore_iter = -1, name = 'ternary_even_shift'):
     initial_pt = ps.PartitionSet(initial_pt)
     final_pt = m.moore_by_parts(initial_pt, machine, n_iter = moore_iter)
 
-    if (moore_iter == -1):
-        with open('../dcgram_files/{}/results/machines/dcgram/before_redefine/dcgram_D{}_K{}.yaml'.format(name, D, K), 'w') as f:
-            yaml.dump(final_pt, f)
-    else:
-        with open('../dcgram_files/{}/results/machines/dcgram/before_redefine/dcgram_D{}_K{}_iter{}.yaml'.format(name, D, K, moore_iter), 'w') as f:
-            yaml.dump(final_pt, f)
+    with open('../dcgram_files/{}/results/machines/dcgram/before_redefine/\dcgram_D{}_K{}{}.yaml'.format(name, D, K, moore_label), 'w') as f:
+        yaml.dump(final_pt, f)
 
     new_pt = final_pt.redefine_partition()
-    if (moore_iter == -1):
-        with open('../dcgram_files/{}/results/machines/dcgram/dcgram_D{}_K{}.yaml'\
-        .format(name, D, K), 'w') as f:
-            yaml.dump(new_pt, f)
-    else:
-        with open('../dcgram_files/{}/results/machines/dcgram/dcgram_D{}_K{}_iter{}.yaml'\
-        .format(name, D, K, moore_iter), 'w') as f:
-            yaml.dump(new_pt, f)
+
+    with open('../dcgram_files/{}/results/machines/dcgram/dcgram_D{}_K{}{}.yaml'\
+    .format(name, D, K, moore_label), 'w') as f:
+        yaml.dump(new_pt, f)
 
     return new_pt
 
