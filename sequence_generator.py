2c52a7e6f74ded676dea386c8f8a881edc35cf7a
Diego Hamilton <diegomhamilton@github.com>
2018-05-02 11:50:29 -0300

    Optimize sequence generation The new function receives machine as a dict = {state: outedges} *execution time (S = 10e6):     - before: 170s     - after: 52s

--- sequence_generator.py
+++ sequence_generator.py
@@ -1,6 +1,7 @@
 import numpy as np
 import dmarkov as dmkv
 import random
+from timeit import default_timer as timer
 
 '''
 Name: generate_sequence
@@ -40,5 +41,26 @@ def generate_sequence(machine, L):
         for state in machine.states:
             if (state.name == curr_state_name):
                 curr_state = state
+    return sequence
 
+
+# thinking machine as a dict {state: outedge}
+def generate_sequence_dict(machine, L):
+    sequence = ''
+    # Starts in machine's first state
+    curr_state_name = list(machine.keys())[0]
+
+    #Generate a L length sequence from DMarkov with D = curr_d
+    for i in range(L):
+        # Set data parameters
+        labels = [oedge[0] for oedge in machine[curr_state_name]]
+        probabilities = [oedge[-1] for oedge in machine[curr_state_name]]
+        # Weight formatting
+        probabilities = [int(p * 10e16) for p in probabilities]
+        # Chooses next state
+        label = random.choices(labels, probabilities)[0]
+        # print(f'Label = {label}')
+        sequence = sequence + label
+        # Goes to next state
+        curr_state_name = [oedge[1] for oedge in machine[curr_state_name] if oedge[0] == label[0]][0]
     return sequence
