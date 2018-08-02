ab8b1857d13677b497ccac5afc01013c758231aa
Diego Hamilton <diegomhamilton@github.com>
2018-08-02 00:38:23 -0300

    Implement Probabilities Transition Matrix and State Occupation vector calculation

---
+++ eigenvectorcalcs.py
@@ -0,0 +1,23 @@
+import numpy as np
+from numpy.linalg import eig
+
+def near(a, b, rtol = 1e-5, atol = 1e-8):
+    return np.abs(a-b)<(atol+rtol*np.abs(b))
+
+def trans_prob_matrix(machine):
+    n = len(machine.states)
+    P = np.zeros((n, n))
+    st_names = [st.name for st in machine.states]
+    idxs = {k: v for v, k in enumerate(st_names)}
+    
+    for st in machine.states:
+        cur_st = idxs[st.name]
+        for oedg in st.outedges:
+            next_st = idxs[oedg[1]]
+            P[cur_st][next_st] += oedg[-1]
+    return P
+
+def occup_vector(machine):
+    P = trans_prob_matrix(machine)
+    values, vectors = eig(P)
+    return vectors[near(values, 1)]
