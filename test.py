df48ac6dbf901d458f821b27a23c13c521fa5d91
Diego Hamilton <diegomhamilton@github.com>
2018-12-23 23:17:50 -0300

    Test file

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-import save_plot as sp
+import dcgram
 
-sp.save_plot(krange = range(3,6))
+label_size = 1
+# D = 2
+name = 'duffing_equation'
+krange = [3, 5, 7]
+L = 1250000
+
+for D in [2]:
+    dcgram.DCGraM(name=name, D=D, krange=krange, load_original_sequence=False, load_machines=True, calc_metrics=True, L = L)
